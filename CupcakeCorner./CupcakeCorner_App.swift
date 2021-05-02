bebf9607f8d6cc7fa2ba5dc3307cbf57e2c82fc6
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-05-02 11:59:40 +0200

    Initial Commit

---
+++ CupcakeCorner./CupcakeCorner_App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_App.swift
+//  CupcakeCorner.
+//
+//  Created by sanmi_personal on 02/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
